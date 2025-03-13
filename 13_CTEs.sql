1cee0afc7891b8332e139e1b1f445b08eef823ba
sahana <sahanatest6@gmail.com>
2025-03-13 22:04:33 +0530

    CTEs - advance

---
+++ 13_CTEs.sql
@@ -0,0 +1,78 @@
+-- Using Common Table Expressions (CTE)
+-- A CTE allows you to define a subquery block that can be referenced within the main query. 
+-- It is particularly useful for recursive queries or queries that require referencing a higher level
+
+-- First, CTEs start using a "With" Keyword. Now we get to name this CTE
+-- Then we say as and within the parenthesis we build our subquery/table we want
+
+WITH CTE_Example AS 
+(
+SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary), AVG(salary)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+-- directly after using it we can query the CTE
+SELECT *
+FROM CTE_Example;
+
+
+-- Now if I come down here, it won't work because it's not using the same syntax
+SELECT *
+FROM CTE_Example;
+
+
+
+-- Now we can use the columns within this CTE to do calculations on this data that
+
+WITH CTE_Example AS 
+(
+SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+-- use back ticks to specify the table names  - without them it doesn't work
+SELECT gender, ROUND(AVG(`SUM(salary)`/`COUNT(salary)`),2)
+FROM CTE_Example
+GROUP BY gender;
+
+
+
+-- to create multiple CTEs with just one With Expression
+
+WITH CTE_Example AS 
+(
+SELECT employee_id, gender, birth_date
+FROM employee_demographics dem
+WHERE birth_date > '1985-01-01'
+), -- just have to separate by using a comma
+CTE_Example2 AS 
+(
+SELECT employee_id, salary
+FROM parks_and_recreation.employee_salary
+WHERE salary >= 50000
+)
+-- Now if we change this a bit, we can join these two CTEs together
+SELECT *
+FROM CTE_Example cte1
+LEFT JOIN CTE_Example2 cte2
+	ON cte1. employee_id = cte2. employee_id;
+
+
+-- renaming the columns in the CTE
+-- We had to use tick marks because of the column names
+
+WITH CTE_Example (gender, sum_salary, min_salary, max_salary, count_salary) AS 
+(
+SELECT gender, SUM(salary), MIN(salary), MAX(salary), COUNT(salary)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+)
+SELECT gender, ROUND(AVG(sum_salary/count_salary),2)
+FROM CTE_Example
+GROUP BY gender;
